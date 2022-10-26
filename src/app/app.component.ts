c4c2a05672c8acec5dc50fda50c999508c1a9d59
Jez Reel Maghuyop <jez.reel.maghuyop@kongsbergdigital.com>
2022-10-26 05:20:22 +0200

    create widget builder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
 import { Component } from '@angular/core';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'widget-builder';
+
+  //#region Custom Table Config
+  myTableHeaderStyle = 'bg-orange-500';
+  myHeader = ['Speaker', 'Country'];
+  myBody = [
+    { name: 'Nick', country: 'Norway' },
+    { name: 'Devlin', country: 'Norway' },
+  ];
+
+  myActions = [{ name: 'View', action: 'View' }];
+
+  //#endregion
+
+  //#region Custom Table Variables
+  tableHeader!: string;
+  tableBody!: string;
+  tableHeaderStyle!: any;
+  generatedTableConfigText!: string;
+  generatedTableConfig!: SafeHtml;
+  //#endregion
+
+  constructor(private sanitized: DomSanitizer) {}
+
+  interceptAction(event: any): void {
+    console.log(event);
+  }
+
+  generate(): void {
+    const processHeader = JSON.stringify(this.tableHeader.split(','));
+    this.generatedTableConfigText = `
+    <script src='https://softconstorage.blob.core.windows.net/widgets/softcon-table.js'></script>
+    <custom-table table-header-class='${this.tableHeaderStyle}' headers='${processHeader}' body-data='${this.tableBody}'></custom-table>`;
+
+    this.generatedTableConfig = this.sanitized.bypassSecurityTrustHtml(
+      this.generatedTableConfigText
+    );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MainMenuComponent } from './shared';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, MainMenuComponent],
+  imports: [BrowserModule, ReactiveFormsModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class AppModule { }
+export class AppModule {}
